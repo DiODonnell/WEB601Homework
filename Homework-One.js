e2993c6a013207e5262724441f2ec4d3b31d9f7f
DiODonnell <dianeodonnell7@gmail.com>
2019-08-11 07:32:28 +1200

    Homework one completed

---
+++ Homework-One.js
@@ -0,0 +1,7 @@
+const addTwoNumbers = (numOne, numTwo) => numOne + numTwo;
+const firstSum = addTwoNumbers(5, 4)
+console.log(firstSum)
+
+const remainderTwoNumbers = (numOne, numTwo) => numOne % numTwo;
+const firstRemainder = remainderTwoNumbers(7, 5)
+console.log(firstRemainder)
